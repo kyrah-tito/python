46477c1ac13238e5d4aca679b09099ca24ede620
Kyrah <kyrahtito100@gmail.com>
2025-04-03 12:15:43 +1300

    added lists

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-print("hello")
-print("hi")
+print("Hello, welcome")
+first_name = input("Please enter your first name: ")
+persons_age = int(input("Please enter your age: "))
+meal_list = ["Standard","Vegetarian","Dairy_Free","No meal"]
+activity_list = ["Music Jam Session","Science Experiments Lab","Sports Leadership Training"]
+other_list = [2,3,4,"easy","moderate","challenging","$5 fee","$10 fee","$12 fee"]
+list_cut = 0
+list_cut0 = 0
+list_cut1 = 0
+fee = 5
+meal_fee = 7
+print("Choose a activity")
+print(f'1. {activity_list[0]} ({other_list[0]} hours, {other_list[3]}, {other_list[6]})')
